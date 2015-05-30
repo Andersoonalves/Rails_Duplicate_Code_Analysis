84ed4bfbb794319dbe03e43c0715cb501806484d
anderson <andersoonalvess@gmail.com>
2015-05-29 22:47:11 -0300

    Five entities with distinct field

---
+++ app/views/employees/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @employee, :id, :data, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524174619) do
+ActiveRecord::Schema.define(version: 20150530014140) do
 
   create_table "companies", force: :cascade do |t|
     t.integer  "numberOfEmployees", limit: 4
@@ -19,10 +19,28 @@ ActiveRecord::Schema.define(version: 20150524174619) do
     t.datetime "updated_at",                  null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.datetime "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.boolean  "available",  limit: 1
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
   create_table "people", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.text     "addres",     limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
