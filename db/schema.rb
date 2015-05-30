c3acf9bd9ac7cedd9d9bb78a94e729d0b69d0a9b
anderson <andersoonalvess@gmail.com>
2015-05-29 22:14:07 -0300

    Six entities without fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530010754) do
+ActiveRecord::Schema.define(version: 20150530011300) do
 
   create_table "companies", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20150530010754) do
   end
 
   create_table "developers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
